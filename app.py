37549196989724b7916c62ef639cc5656d34cd2c
Jesus Gonzalez <Jesus.gonzalez@mitechnologiesinc.com>
2025-09-26 10:29:23 -0700

    Redirect home route to index and add initial index.html template

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('home.html', title='Home Page')
+    return redirect(url_for('index'))
+
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html', title='Index Page')
 
 @app.route('/login', methods=['GET'])
 def login():
