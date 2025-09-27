8b58a9b50c662ba3846d9bb7d13d1372756b63c5
pedroRuaH <pedro@uniortholab.com>
2025-09-27 09:56:49 -0700

    update

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
-# importar dependencias
-from flask import Flask
-from config import Config
-from models import db
-from auth.routes import auth_bp
-from main.routes import main_bp
-from flask import render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    db.init_app(app)
+app = Flask(__name__)
 
-    with app.app_context():
-        db.create_all()
+@app.route('/', methods=['GET'])
+def home():
+    return redirect(url_for('JesusDev'))
 
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(main_bp)
+@app.route('/JesusDev', methods=['GET'])
+def JesusDev():
+    return render_template('JesusDev.html', title='JesusDev Page')
 
-    @app.route('/', methods=['GET'])
-    def home():
-        return render_template('home.html')
-    
-    @app.route('/dashboard')
-    def dashboard():
-        if not session.get('user_authenticated'):
-            return redirect(url_for('auth.login'))
-        return render_template('dashboard.html', username=session.get('username'))
-    
-    return app
- 
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html', title='Login')
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
