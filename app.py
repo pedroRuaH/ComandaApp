da8d5de57e62c9997d260138f85bc43fd0a105f8
Mario <mario.quinonescastro@cesunbc.edu.mx>
2025-09-26 22:03:19 -0700

    Add MarioDev page and image; update routes in app.py

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
+def MarioDev():
+    return render_template('MarioDev.html', title='MarioDev')
+
+@app.route('/home', methods=['GET'])
 def home():
     return render_template('home.html', title='Home Page')
+@app.route('/MarioDev', methods=['GET'])
+def mario_dev():
+    return render_template('MarioDev.html', title='MarioDev')
 
 @app.route('/login', methods=['GET'])
 def login():
@@ -12,3 +19,5 @@ def login():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
